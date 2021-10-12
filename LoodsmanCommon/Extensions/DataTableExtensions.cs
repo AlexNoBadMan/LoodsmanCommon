1ac496c0831a712c83a6c41ea408eb8a83bec751
wosanek <wosanek@gmail.com>
2021-10-12 17:08:46 +0600

    Добавлено расширение DataTable

---
+++ LoodsmanCommon/Extensions/DataTableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoodsmanCommon.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static IEnumerable<DataRow> GetRows(this DataTable dataTable)
+        {
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+                yield return dataTable.Rows[i];
+        }
+    }
+}
