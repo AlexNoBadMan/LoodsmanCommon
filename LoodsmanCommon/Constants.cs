59a41f345d9869e351a833e09a17a27b74690a5d
wosanek <wosanek@gmail.com>
2021-10-12 15:09:32 +0600

    Добавлен файл констант

---
+++ LoodsmanCommon/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace LoodsmanCommon
+{
+    public static class Constants
+    {
+        public const string DEFAULT_INSERT_NEW_VERSION = " ";
+        public const string DEFAULT_NEW_VERSION = "1.0";
+        public const string DEFAULT_NEW_NO_VERSION = "";
+    }
+}
