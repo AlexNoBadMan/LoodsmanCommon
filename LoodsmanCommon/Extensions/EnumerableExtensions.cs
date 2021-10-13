258f81da927a574b7be255a5b244742df2ae86d5
wosanek <wosanek@gmail.com>
2021-10-13 11:46:31 +0600

    Добавлен метод расширения ToReadOnlyList

---
+++ LoodsmanCommon/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LoodsmanCommon.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> source) => new ReadOnlyCollection<T>(source.ToArray());
+    }
+}
