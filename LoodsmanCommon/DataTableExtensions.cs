42be18044e112489a47c9b2216d76a2c53350678
wosanek <wosanek@gmail.com>
2021-11-28 13:09:30 +0600

    Для DataTable добавлен метод расширения FirstOrDefalut аналог Linq

--- LoodsmanCommon/DataTableExtensions.cs
+++ LoodsmanCommon/DataTableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Data;
 
@@ -33,6 +34,33 @@ namespace LoodsmanCommon
             for (int i = 0; i < dataTable.Rows.Count; i++)
                 if (predicate(dataTable.Rows[i]))
                     yield return dataTable.Rows[i];
+        }
+
+        /// <summary>
+        /// Возвращает первый элемент последовательности, удовлетворяющий указанному условию, или значение по умолчанию, если ни одного такого элемента не найдено.
+        /// </summary>
+        /// <param name="dataTable">Таблица данных.</param>
+        /// <param name="predicate">Функция для проверки каждого элемента исходной последовательности условие.</param>
+        /// <returns>Возвращает первый <see cref="DataRow"/>, удовлетворяющий указанному условию (<paramref name="predicate"/>) или значение по умолчанию.</returns>
+        public static DataRow FirstOrDefault(this DataTable dataTable, Func<DataRow, bool> predicate)
+        {
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+                if (predicate(dataTable.Rows[i]))
+                    return dataTable.Rows[i];
+            
+            return null;
+        }
+
+        /// <summary>
+        /// Возвращает первый элемент последовательности, удовлетворяющий указанному условию, или значение по умолчанию, если ни одного такого элемента не найдено.
+        /// </summary>
+        /// <param name="dataTable">Таблица данных.</param>
+        /// <param name="predicate">Функция для проверки каждого элемента исходной последовательности условие.</param>
+        /// <param name="columnName">Наименование заголовка столбца, значение которого необходимо получить.</param>
+        /// <returns>Возвращает значение указанного столбца (<paramref name="columnName"/>), удовлетворяющий указанному условию (<paramref name="predicate"/>) или значение по умолчанию.</returns>
+        public static object FirstOrDefault(this DataTable dataTable, Func<DataRow, bool> predicate, string columnName)
+        {
+            return dataTable.FirstOrDefault(predicate)?[columnName];
         }
     }
 }
