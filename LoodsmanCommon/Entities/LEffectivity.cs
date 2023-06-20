7dcc90adb7e7cd5ab3e03bc28aecba4ff38ced02
wosanek <wosanek@gmail.com>
2023-06-20 14:42:02 +0600

    Предварительная реализация применяемости LEffectivity

---
+++ LoodsmanCommon/Entities/LEffectivity.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+
+//namespace LoodsmanCommon
+//{
+//  public class LEffectivity : LAttributeOwner
+//  {
+//    public LEffectivity(int id, string name, LEffectivityTypeInfo effTypeInfo) : base(id, name)
+//    {
+//      EffTypeInfo = effTypeInfo;
+//    }
+
+//    public LEffectivityTypeInfo EffTypeInfo { get; }
+
+
+//    public override void UpdateAttribute(string name, object value, LMeasureUnit unit)
+//    {
+//            Ascon.Plm.Loodsman.PluginSDK.INetPluginCall call = null;
+//      call.Native_SetVersionEffAttrValue(Id, name, value);
+//    }
+
+//    protected override NamedEntityCollection<LAttribute> GetAttributes()
+//    {
+//      foreach (var item in EffTypeInfo.Attributes)
+//      {
+
+//      }
+//      throw new NotImplementedException();
+//    }
+//  }
+//}
---
+++ LoodsmanCommon/Entities/Meta/LEffectivityTypeInfo.cs
@@ -0,0 +1,21 @@
+﻿//using Ascon.Plm.Loodsman.PluginSDK;
+//using System.Collections.Generic;
+
+//namespace LoodsmanCommon
+//{
+//  public class LEffectivityTypeInfo : Entity
+//  {
+//    private readonly ILoodsmanMeta _meta;
+//    private readonly INetPluginCall _pc;
+//    private IEnumerable<LAttributeInfo> _attributes;
+
+//    public LEffectivityTypeInfo(ILoodsmanMeta meta, INetPluginCall pc, int id, string name) : base(id, name)
+//    {
+//      _meta = meta;
+//      _pc = pc;
+//    }
+
+//    /// <summary> Список возможных атрибутов типа применияемости. </summary>
+//    public IEnumerable<LAttributeInfo> Attributes => _attributes ??= _pc.Native_GetEffTypeAttributes(Id).Select(x => _meta.Attributes[x.ATTR_NAME()]);
+//  }
+//}
